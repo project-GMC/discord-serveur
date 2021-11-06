7c67402cee3cf8469b8219e7f9feb77b9369e609
KhRania <rkhadaier@gmail.com>
2021-11-06 15:24:54 +0100

    create ci pipeline

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name bot -e DISCORD_TOKEN="OTA0Mzk2MDUzODQ2MDM2NTUw.YX66dg.NMrvZkvm7Nl1I4EjxC4CXhTUd_M" discord_bot_quotes:latest
+docker run -d --name bot -e DISCORD_TOKEN=${DISCORD_TOKEN} discord_bot_quotes:latest
