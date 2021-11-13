9356d9fdf497d935d1f0001af671ae030a6bc32a
KhRania <rkhadaier@gmail.com>
2021-11-13 08:13:53 +0100

    update bot

--- src/bot.py
+++ src/bot.py
@@ -26,7 +26,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!quote'):
+    if message.content.startswith('$quote'):
         quote=get_quote()
         await message.channel.send(quote)
 
